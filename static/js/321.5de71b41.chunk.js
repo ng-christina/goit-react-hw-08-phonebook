c393e0a9b8ec6c8202e996080badde71ab9d568f
ng-christina <118376146+ng-christina@users.noreply.github.com>
2023-09-04 11:19:21 +0000

    Deploying to gh-pages from @ ng-christina/goit-react-hw-08-phonebook@0bfa5b047c594fc8c9124642a4cb86118d741bb9 🚀

---
+++ static/js/321.5de71b41.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_hw_08_phonebook=self.webpackChunkreact_hw_08_phonebook||[]).push([[321],{321:function(e,t,n){n.r(t);var o=n(2791),c=n(2692),i=n(184);t.default=function(){return(0,o.useEffect)((function(){document.title="Home"}),[]),(0,i.jsx)("div",{children:(0,i.jsx)("h1",{className:c.Z.title,children:"Phonebook"})})}},2692:function(e,t){t.Z={title:"Contact_title__wJYPb"}}}]);
+//# sourceMappingURL=321.5de71b41.chunk.js.map
